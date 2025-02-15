762397090f627ddee82dc51f2b7d69353f5d9a81
baron-de-montblanc <jade_ducharme@brown.edu>
2025-02-15 14:01:47 -0500

    front page and navigation fully functional

---
+++ assets/js/load-templates.js
@@ -0,0 +1,40 @@
+async function loadTemplate(id, file, callback) {
+    let element = document.getElementById(id);
+    if (element) {
+        let response = await fetch(file);
+        if (response.ok) {
+            element.innerHTML = await response.text();
+            if (callback) callback(); // Run callback after template loads
+        } else {
+            console.error(`Error loading ${file}: ${response.statusText}`);
+        }
+    }
+}
+
+// jQuery-based function to initialize dropdown menus
+function initializeNavbarDropdowns() {
+    console.log("Re-initializing navbar dropdowns...");
+    
+    $(document).ready(function () {
+        $('.sub-menu ul').hide();
+    
+        $(".sub-menu a").click(function (e) {
+            let hasSubmenu = $(this).parent(".sub-menu").children("ul").length > 0;
+    
+            if (hasSubmenu) {
+                e.preventDefault();  // Prevents jumping to # ONLY if there's a submenu
+                $(this).parent(".sub-menu").children("ul").slideToggle(100);
+                $(this).find(".right").toggleClass("fa-caret-right fa-caret-down");
+            }
+        });
+    });
+    
+}
+
+// Load navbar and reapply jQuery event listeners
+document.addEventListener("DOMContentLoaded", function () {
+    loadTemplate("header-container", "../partials/header.html");
+    loadTemplate("navbar-template", "../partials/navbar-template.html", initializeNavbarDropdowns);
+});
+
+
